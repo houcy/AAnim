a4a8bf8ffdb2286b15f7227b9f539e1ad26f8261
Joy <zhuozhuo530@gmail.com>
2021-10-13 22:32:47 -0700

    Refactor TreeNode class

--- practice/tree.py
+++ practice/tree.py
@@ -1,7 +1,7 @@
 from manimlib import *
 from collections import deque
 
-# try run $ manimgl tree.py BuildTree
+# try run $ manimgl tree.py BuildHeap
 
 MIN = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 MAX = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -47,6 +47,9 @@ class TreeNode:
         return root
 
     def size(self):
+        """
+        Get the size of the tree
+        """
         size = 1
         if self.left:
             size += self.left.size()
@@ -54,29 +57,28 @@ class TreeNode:
             size += self.right.size()
         return size
 
-
-class HeapNode(TreeNode):
-    """
-    Inherit from TreeNode and add more attributes about array representation
-    """
-    def __init__(self, value):
-        self.array_position_x = 0
-        self.array_position_y = 0
-        self.array_object = None
-        TreeNode.__init__(self, value)
-
-
-class BuildTree(Scene):
-    ###################################################
-    # Methods that convert a list to tree and mobjects #
-    ###################################################
-
-    def get_offset(self, root):
+    def unpack(self):
+        """
+        Unpack the tree to a list of nodes in level order
+        """
+        stack = [self]
+        all = [self]
+        while stack:
+            node = stack.pop()
+            if node.left:
+                stack.append(node.left)
+                all.append(node.left)
+            if node.right:
+                stack.append(node.right)
+                all.append(node.right)
+        return all
+        
+    def _get_offset(self):
         """
         Calculate the correct offset (so that the node in the bottom level doesn’t overlap)
         """
         # Hardcode the offset for different range = (how many nodes in total, corresponding offset value)
-        size = root.size()
+        size = self.size()
         hardcode_standard = [(3, 0.5), (7, 1), (15, 2), (32, 3), (63, 4)]
         if size > 63:
             print("The max length we support is 63. Now the lengh is:", size)
@@ -85,93 +87,120 @@ class BuildTree(Scene):
             if size <= cutoff:
                 return offset
 
-
-    def create_node_object(self, node):
+    def _create_node_object(self):
         """
         Convert a TreeNode to an MObject so that it shows on the canvas
         """
         radius = 0.3
         font_size = 0.5 
         circle = Circle(radius=radius).set_stroke(color=WHITE, width=1).set_fill(WHITE, opacity=1.0)
-        text = Text(str(node.value), font="Open Sans", weight="THIN", color=BLACK).scale(font_size)
+        text = Text(str(self.value), font="Open Sans", weight="THIN", color=BLACK).scale(font_size)
         text.add_updater(lambda m: m.move_to(circle.get_center())) # Place the text at the center of the circle
         # If the node is on the left side of the root
-        if node.position_x < 0:
-            return VGroup(circle, text).shift(LEFT * abs(node.position_x) + DOWN * node.position_y)
+        if self.position_x < 0:
+            return VGroup(circle, text).shift(LEFT * abs(self.position_x) + DOWN * self.position_y)
         # If the node is on the right side of the root
         else:
-            return VGroup(circle, text).shift(RIGHT * abs(node.position_x) + DOWN * node.position_y)
+            return VGroup(circle, text).shift(RIGHT * abs(self.position_x) + DOWN * self.position_y)
 
-
-    def populate_position(self, node, offset):
+    def _populate_position_and_objects(self, offset):
         """
-        Calculate the position of each treenode and save in the treenode object
+        Calculate the position of the treenode the its children and save in the treenode object
+        and create the node object for each node
         """
-        if node.parent is None:
-            if node.left:
-                node.left.offset = -offset
-                self.populate_position(node.left, offset)
-            if node.right:
-                node.right.offset = offset
-                self.populate_position(node.right, offset)
+        if self.parent is None:
+            if self.left:
+                self.left.offset = -offset
+                self.left._populate_position_and_objects(offset)
+            if self.right:
+                self.right.offset = offset
+                self.right._populate_position_and_objects(offset)
         else:
-            parent_x = node.parent.position_x
-            parent_y = node.parent.position_y
-            node.position_x = parent_x + node.offset
-            node.position_y = parent_y + 1
-            if node.left:
-                node.left.offset = -abs(node.offset) / 2
-                self.populate_position(node.left, offset)
-            if node.right:
-                node.right.offset = abs(node.offset) / 2
-                self.populate_position(node.right, offset)
+            parent_x = self.parent.position_x
+            parent_y = self.parent.position_y
+            self.position_x = parent_x + self.offset
+            self.position_y = parent_y + 1
+            if self.left:
+                self.left.offset = -abs(self.offset) / 2
+                self.left._populate_position_and_objects(offset)
+            if self.right:
+                self.right.offset = abs(self.offset) / 2
+                self.right._populate_position_and_objects(offset)
         # Convert node to an MObject so that it can be showed on canvas
-        node.object = self.create_node_object(node)
+        self.object = self._create_node_object()
+            
 
-
-    def get_all_node_objects(self, node, node_objects):
+    def _get_all_node_objects(self, node_objects):
         """
         Convert tree nodes to a list of (circle+text) MObject
         """
-        node_objects.append(node.object)
-        if node.left:
-            self.get_all_node_objects(node.left, node_objects)
-        if node.right:
-            self.get_all_node_objects(node.right, node_objects)
+        node_objects.append(self.object)
+        if self.left:
+            self.left._get_all_node_objects(node_objects)
+        if self.right:
+            self.right._get_all_node_objects(node_objects)
 
-
-    def get_all_line_objects(self, node, line_objects):
+    def _get_all_line_objects(self, line_objects):
         """
         Convert the lines to a list of line MObject
         """
-        if node.left:
-            line_objects.append(Line(node.object.get_center(), node.left.object.get_center()).set_stroke(color=WHITE, width=1))
-            self.get_all_line_objects(node.left, line_objects)
-        if node.right:
-            line_objects.append(Line(node.object.get_center(), node.right.object.get_center()).set_stroke(color=WHITE, width=1))
-            self.get_all_line_objects(node.right, line_objects)
+        if self.left:
+            line_objects.append(Line(self.object.get_center(), self.left.object.get_center()).set_stroke(color=WHITE, width=1))
+            self.left._get_all_line_objects(line_objects)
+        if self.right:
+            line_objects.append(Line(self.object.get_center(), self.right.object.get_center()).set_stroke(color=WHITE, width=1))
+            self.right._get_all_line_objects(line_objects)
 
-
-    def tree_to_tree_mobjects(self, root):
+    def tree_to_mobjects(self):
         """
         Convert an abstract tree object to its mobject representation.
         Return a list of node object(Mobject), a list of lines(Mobject). 
         """
         # Check the length of the list (should be <=63) and output the offset based on the length
         # offset is the half of the distance between root.left and root.right
-        offset = self.get_offset(root)
+        offset = self._get_offset()
         if not offset:
             return
         # Fill in the position of each node, pass the offset to draw nodes
-        self.populate_position(root, offset)
+        self._populate_position_and_objects(offset)
         # Convert the tree to a list of (circle+text) MObject
         node_objects = []
-        self.get_all_node_objects(root, node_objects)
+        self._get_all_node_objects(node_objects)
         # Convert the lines to a list of line MObject
         line_objects = []
-        self.get_all_line_objects(root, line_objects)
+        self._get_all_line_objects(line_objects)
         return node_objects, line_objects
 
+
+class HeapNode(TreeNode):
+    """
+    Inherit from TreeNode and add more attributes about array representation
+    """
+    def __init__(self, value):
+        super().__init__(value)
+        self.array_position_x = 0
+        self.array_position_y = 1
+        self.array_object = None
+
+
+    # def _populate_array_position_and_objects(self, start):
+    #     """
+    #     Calculate the position of the treenode the its children and save in the treenode object
+    #     and create the node object for each node
+    #     """
+        
+    
+    # def draw_array(self):
+    #     """
+    #     Create an a copy of all node objects and save in an array
+    #     """
+    #     self._create_array_object()
+
+
+class BuildHeap(Scene):
+    ###################################################
+    # DRAW A TREE #
+    ###################################################
 
     def draw_tree(self, node_objects, line_objects):
         """
@@ -236,17 +265,7 @@ class BuildTree(Scene):
         """
         Build a heap started at root
         """
-        stack = [root]
-        all = [root]
-        # Unpack the tree to a list of nodes in level order
-        while stack:
-            node = stack.pop()
-            if node.left:
-                stack.append(node.left)
-                all.append(node.left)
-            if node.right:
-                stack.append(node.right)
-                all.append(node.right)
+        all = root.unpack()
         # Iterate all nodes from bottom to top and heapify each node
         while all:
             curr_node = all.pop()
@@ -259,12 +278,15 @@ class BuildTree(Scene):
         """
         This is the main function called by manim
         """
-        # To build a heap tree
+        # To build a tree (initial heap that hasn't been heapified)
         root = HeapNode.from_array(MAX)
-        node_objects, line_objects = self.tree_to_tree_mobjects(root)
+        node_objects, line_objects = root.tree_to_mobjects()
+
+        # To draw the tree (initial heap that hasn't been heapified)
         self.draw_tree(node_objects, line_objects)
 
-        # To build a heap by the tree 
+        # To animate build heap (apply heapify)
         self.build_heap(root, is_min_heap=False)
+
 
 
