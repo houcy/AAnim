1f29873f434f91bf00f6072447904dc03ece1b4f
Joy <zhuozhuo530@gmail.com>
2022-01-18 11:31:09 -0800

    Fix code block height issue

--- practice/code_block.py
+++ practice/code_block.py
@@ -16,6 +16,13 @@ TEST_CODE = """EXTRACT-FIRST(A) {
     HeapifyDown(A, 1)
 }
 """
+
+CODE_FOR_BUILD = """BUILD-HEAP(A[1,...,n]) {
+    for i = floor(n/2) downto 1
+        HeapifyDown(A, i)
+}
+"""
+
 CODE_LINE_SPACING = 1
 
 class CodeBlock():
@@ -31,11 +38,7 @@ class CodeBlock():
         self.total_lines = len(self.code.code_json)
         self.height = self.top - self.bottom
         self.width = self.right - self.left
-        # self.line_height = (self.height - CODE_LINE_SPACING * (self.total_lines - 1)) / self.total_lines
-        self.line_height = self.height / self.total_lines
-        # print("self.height", self.height)
-        # print("self.total_lines", self.total_lines)
-        # print("self.line_height", self.line_height)
+        self.line_height = self.height / (self.total_lines + self.total_lines*CODE_LINE_SPACING - CODE_LINE_SPACING)
         self.highlight_rect = None
         self.title = None
 
@@ -44,28 +47,28 @@ class CodeBlock():
         return self.title
 
     def highlight(self, line_number):
-        if line_number > self.total_lines:
+        if line_number > self.total_lines or line_number <= 0:
+            print("Error: line number out of the range")
             return
-        y_position = self.top - (line_number-0.5) * self.line_height
+        # if not self.highlight_rect:
+        #     y_position = self.top - (line_number-0.5) * self.line_height
+        #     self.highlight_rect = Rectangle(width=self.width+CODE_BLOCK_WIDTH_PADDING, height=self.line_height+CODE_BLOCK_HEIGHT_PADDING).set_stroke(color=YELLOW, width=2).shift(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
+        #     return FadeIn(self.highlight_rect)
+        # else:
+        #     y_position = self.top - (self.line_height + self.line_height * CODE_LINE_SPACING) * (line_number - 1) - 0.5 * self.line_height
+        #     return self.highlight_rect.animate.move_to(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
+
+        y_position = self.top - (self.line_height + self.line_height * CODE_LINE_SPACING) * (line_number - 1) - 0.5 * self.line_height
         if not self.highlight_rect:
-            self.highlight_rect = Rectangle(width=self.width+CODE_BLOCK_RECTANGLE_WIDTH, height=self.line_height+CODE_BLOCK_RECTANGLE_HEIGHT).set_stroke(color=YELLOW, width=2).shift(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
+            self.highlight_rect = Rectangle(width=self.width+CODE_BLOCK_WIDTH_PADDING, height=self.line_height+CODE_BLOCK_HEIGHT_PADDING).set_stroke(color=YELLOW, width=2).shift(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
             return FadeIn(self.highlight_rect)
         else:
             return self.highlight_rect.animate.move_to(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
-        # if line_number > self.total_lines or line_number <= 0:
-        #     print("Error: line number out of the range")
-        #     return
-        # if not self.highlight_rect:
-        #     y_position = self.top - (line_number-0.5) * self.line_height
-        #     self.highlight_rect = Rectangle(width=self.width+CODE_BLOCK_RECTANGLE_WIDTH, height=self.line_height+CODE_BLOCK_RECTANGLE_HEIGHT).set_stroke(color=YELLOW, width=2).shift(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
-        #     return FadeIn(self.highlight_rect)
-        # else:
-        #     y_position = self.top - (self.line_height + CODE_LINE_SPACING) * (line_number - 1) - 0.5 * self.line_height
-        #     return self.highlight_rect.animate.move_to(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
+
 
 class CodeScene(Scene):
     def construct(self):
-        code2 = CodeBlock(TEST_CODE)
+        code2 = CodeBlock(CODE_FOR_BUILD)
         self.play(FadeIn(code2.code))
-        for i in range(1, 12):
+        for i in range(1, 4):
             self.play(code2.highlight(i))
