3bb2e4a222a45989e292f14304fb1b0f05983578
Joy <zhuozhuo530@gmail.com>
2022-02-26 13:43:55 -0800

    FEATURE: highlight multiple lines of code in one highlight box

--- practice/code_block.py
+++ practice/code_block.py
@@ -33,9 +33,11 @@ class CodeBlock():
             self.height = self.top - self.bottom
             self.width = self.right - self.left
             self.line_height = self.height / (self.total_lines + self.total_lines*CODE_LINE_SPACING - CODE_LINE_SPACING)
+            self.line_height_include_spacing = self.line_height + self.line_height * CODE_LINE_SPACING
             self.highlight_rect = None
             self.title = None
             self._set_background_color()
+            self.last_n_lines = 1
 
     def _set_background_color(self):
         self.code.background_mobject.set_fill(BACKGROUND)
@@ -44,26 +46,35 @@ class CodeBlock():
         self.title = Text(title, color=LINE_COLOR, font="Karla").scale(TITLE_SIZE).set_z_index(2).next_to(self.code, UP, buff=0.5)
         return self.title
 
-    def highlight(self, line_number):
+    def highlight(self, line_number, n_lines=1):
         """
         To highlight line 3 for example: self.play(code_instance.highlight(3))
         """
-        if line_number > self.total_lines or line_number <= 0:
+        if line_number+n_lines-1 > self.total_lines or line_number <= 0:
             print("Error: line number out of the range")
             return
-        y_position = self.top - (self.line_height + self.line_height * CODE_LINE_SPACING) * (line_number - 1) - 0.5 * self.line_height
+        y_base = self.top - self.line_height_include_spacing * (line_number - 1) - 0.5 * self.line_height
+        y_position = y_base - (n_lines - 1) * (self.line_height_include_spacing / 2)
         if not self.highlight_rect:
-            self.highlight_rect = Rectangle(width=self.width+CODE_BLOCK_WIDTH_PADDING, height=self.line_height+CODE_BLOCK_HEIGHT_PADDING).set_stroke(color=GRAY, width=2).shift(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
+            self.highlight_rect = RoundedRectangle(corner_radius=0.05, width=self.width+CODE_BLOCK_WIDTH_PADDING, height=self.line_height_include_spacing*n_lines+CODE_BLOCK_HEIGHT_PADDING).set_stroke(color=GRAY, width=2).shift(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
+            self.last_n_lines = n_lines
             self.code = VGroup(self.code, self.highlight_rect)
             return FadeIn(self.highlight_rect)
         else:
-            return AnimationGroup(self.highlight_rect.animate.move_to(SHIFT_LEFT_UNIT * LEFT + UP * y_position), Wait(2))
+            if self.last_n_lines == n_lines:
+                return AnimationGroup(self.highlight_rect.animate.move_to(SHIFT_LEFT_UNIT * LEFT + UP * y_position), Wait(2))
+            else:
+                self.last_n_lines = n_lines
+                last_highlight_rect = self.highlight_rect
+                self.highlight_rect = RoundedRectangle(corner_radius=0.05, width=self.width+CODE_BLOCK_WIDTH_PADDING, height=self.line_height_include_spacing*n_lines+CODE_BLOCK_HEIGHT_PADDING).set_stroke(color=GRAY, width=2).shift(SHIFT_LEFT_UNIT * LEFT + UP * y_position)
+                return AnimationGroup(ReplacementTransform(last_highlight_rect, self.highlight_rect), Wait(2))
 
 
 class Test(Scene):
     def construct(self):
         code2 = CodeBlock(TEST_CODE)
         self.play(FadeIn(code2.create_title("hellow")))
-        # self.play(FadeIn(code2.code))
-        # for i in range(1, 4):
-        #     self.play(code2.highlight(i))
+        self.play(FadeIn(code2.code))
+        self.play(code2.highlight(1, 1))
+        self.play(code2.highlight(2, 1))
+        self.play(code2.highlight(4, 2))
