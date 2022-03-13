9a2661702b5bbc3e4fcb723dc74dfab70802d294
Joy <zhuozhuo530@gmail.com>
2022-03-12 17:35:08 -0800

    FEATURE: legend can be rounded rectangle

--- practice/legend.py
+++ practice/legend.py
@@ -1,6 +1,8 @@
 from manim import *
 from style import *
 
+
+
 class Legend:
     def __init__(self, dict):
         """
@@ -8,11 +10,20 @@ class Legend:
         """
         self.animation = None
         inside = VGroup()
-        for color, explanation in dict.items():
-            circle = Circle(color=color, radius=SM_RADIUS).set_fill(color, opacity=1.0)
-            text = Text(str(explanation), color=LINE_COLOR, font=FONT, font_size=LEGEND_SIZE).next_to(circle, RIGHT, buff=2)
-            inside += circle
-            inside += text
+        for feature, explanation in dict.items():
+            if feature[0] == "HIGHLIGHT_ROUNDED_RECTANGLE":
+                command, fill_color, stroke_color = feature
+                if command == "HIGHLIGHT_ROUNDED_RECTANGLE":
+                    rect = RoundedRectangle(corner_radius=0.06, height=2.2*SM_RADIUS, width=2.2*SM_RADIUS).set_fill(fill_color, 1).set_stroke(color=stroke_color)
+                    text = Text(str(explanation), color=LINE_COLOR, font=FONT, font_size=LEGEND_SIZE).next_to(circle, RIGHT, buff=2)
+                    inside += rect
+                    inside += text
+
+            else:
+                circle = Circle(color=feature, radius=SM_RADIUS).set_fill(feature, 1)
+                text = Text(str(explanation), color=LINE_COLOR, font=FONT, font_size=LEGEND_SIZE).next_to(circle, RIGHT, buff=2)
+                inside += circle
+                inside += text
         inside = inside.arrange_in_grid(cols=2, buff=LEGEND_BUFF)
         box = SurroundingRectangle(inside, corner_radius=0, color=LINE_COLOR, stroke_width=LEGEND_STROKE_WIDTH, buff=0.2)
         self.animation = AnimationGroup(FadeIn(inside), Create(box), Wait())
@@ -21,5 +32,5 @@ class Legend:
 class Test(Scene):
     def construct(self):
         self.camera.background_color = BLACK
-        l = Legend({PINK1: "curr level", PINK3: "next level", BLUE1: "finished"})
+        l = Legend({PINK1: "MST so far", ("HIGHLIGHT_ROUNDED_RECTANGLE", PINK1, HIGHLIGHT_STROKE): "v"})
         self.play(l.animation)
