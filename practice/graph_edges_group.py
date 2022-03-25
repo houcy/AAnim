baa40f4a79405970a140a77d2d35704d956fb155
Joy <zhuozhuo530@gmail.com>
2022-03-24 19:14:25 -0700

    MOD: update the highlight/dehighlight to include save_state and restore features

--- practice/graph_edges_group.py
+++ practice/graph_edges_group.py
@@ -4,14 +4,24 @@ from style import *
 
 
 class GraphEdgesGroup:
-    def __init__(self):
+    def __init__(self, edges_array=None):
         self.mobject = VGroup()
+        self.save_state = False
+        if edges_array:
+            for edge in edges_array:
+                self.add(edge)
     
     def add(self, edge):
         self.mobject += edge.mobject["line"]
 
     def highlight(self, color):
-        return AnimationGroup(self.mobject.animate.set_fill(color).set_stroke(color, width=WIDTH+5), Wait())
+        self.mobject.save_state()
+        self.save_state = True
+        return AnimationGroup(self.mobject.animate.set_fill(color).set_stroke(color, width=WIDTH+5), Wait(), lag_ratios=1)
 
     def dehighlight(self, color=GRAY):
-        return AnimationGroup(self.mobject.animate.set_fill(color).set_stroke(color, width=WIDTH), Wait())
+        if self.save_state:
+            self.save_state = False
+            return AnimationGroup(Restore(self.mobject), Wait(), lag_ratios=1)
+        else:
+            return AnimationGroup(self.mobject.animate.set_fill(color).set_stroke(color, width=WIDTH), Wait(), lag_ratios=1)
