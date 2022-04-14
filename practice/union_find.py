25a2819728274af81dc83c786c8b4fd6890b2968
Joy <zhuozhuo530@gmail.com>
2022-04-14 12:26:53 -0700

    MOD: update the animation of highlighting a node such that the stroke color matches with the fill color

--- practice/union_find.py
+++ practice/union_find.py
@@ -1,6 +1,8 @@
 from manim import *
 from style import *
 from graph_edges_group import GraphEdgesGroup
+from color_generator import ColorGenerator
+
 
 class UnionFind:
     def __init__(self, array):
@@ -15,15 +17,15 @@ class UnionFind:
         # if len(LIGHT_COLORS) >= len(array):
         color_index = 0
         animations = []
+        cg = ColorGenerator()
+        color_tuples= cg.vivid_colors(len(array))
         for node in array:
-            if color_index == len(LIGHT_COLORS):
-                color_index = 0
-            node_color = LIGHT_COLORS[color_index]
-            animations.append(node.color(fill_color=node_color))
-            self.color[node] = node_color
+            node_color, stroke_color = color_tuples[color_index]
+            animations.append(node.color(fill_color=node_color, stroke_color=stroke_color))
+            self.color[node] = (node_color, stroke_color)
             color_index += 1
         self.animation = AnimationGroup(*animations, lag_ratio=0.2)
-    
+
     def show_set(self):
         """
         Usually called after initialize an instance (each node is a set)
@@ -44,6 +46,9 @@ class UnionFind:
             for child in node.children:
                 descendants += self.all_descendants(child)
             return descendants
+
+    def all_edges_under_root(self, node):
+        return self.root2edges[node]
     
     def union(self, element_b, element_a, edge=None):
         root_of_a = self.find(element_a)
@@ -57,14 +62,14 @@ class UnionFind:
         root_of_a.parent = root_of_b
         root_of_b.children.append(root_of_a)
         # change the color of a and all descendants to the color of group b
-        root_of_b_color = self.color[root_of_b]
+        root_of_b_color, root_of_b_stroke_color = self.color[root_of_b]
         animations = []
         for descendant in a_and_descendants:
-            animations.append(descendant.color(fill_color=root_of_b_color))
+            animations.append(descendant.color(fill_color=root_of_b_color, stroke_color=root_of_b_stroke_color))
             new_text_mobject = root_of_b.text_mobject.copy().move_to(descendant.text_mobject.get_center())
             animations.append(ReplacementTransform(descendant.text_mobject, new_text_mobject))
             descendant.text_mobject = new_text_mobject
-        self.donimant_color = root_of_b_color
+        self.donimant_color = (root_of_b_color, root_of_b_stroke_color)
         # update b.edges
         self.root2edges[root_of_b] = [edge] + self.root2edges[root_of_b] + self.root2edges[root_of_a]
         if not edge:
